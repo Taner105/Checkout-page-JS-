c2754be36c4892971612b37a34325982eef3ef39
Taner105 <taneraydogan59@gmail.com>
2021-11-07 22:05:22 +0300

    project added.

--- checkout.js
+++ checkout.js
@@ -9,6 +9,8 @@ window.onload = () => {
     window.sessionStorage.setItem("taxRate", taxRate);    //Browser kapandığında sessionın içerisi boşalır
     window.sessionStorage.setItem("shippingPrice", shippingPrice);
 
+    calculateCartTotal(); //sayfa yüklendiği zaman adet bazında totali göster.
+
 }
 
 let quantityControllerDivs = document.getElementsByClassName("quantity-controller");
@@ -16,13 +18,71 @@ console.log(quantityControllerDivs);
 
 [...quantityControllerDivs].forEach((quantityControllerDiv)=>{
     //minus butonuna ulaştım.
+    let quantityP = quantityControllerDiv.querySelector("#product-quantity")
     quantityControllerDiv.firstElementChild.addEventListener("click", ()=>{
-        let quantityP = quantityControllerDiv.querySelector("#product-quantity")
         quantityP.innerText = parseInt(quantityP.innerText) - 1
+
+        if(quantityP.innerText == "0"){
+            alert("product will be removed!")
+            quantityControllerDiv.parentElement.parentElement.remove();
+        }
+        calculateProductTotal(quantityP);
+        
 
     });
     // plus butonuna ulaştım.
-    quantityControllerDiv.firstElementChild.addEventListener("click", ()=>{
+    
+    quantityControllerDiv.lastElementChild.addEventListener("click", ()=>{
+        quantityP.innerText = parseInt(quantityP.innerText) + 1
+        calculateProductTotal(quantityP);
 
     });
 });
+const calculateProductTotal = (quantityP) =>{
+    let productInfoDiv = quantityP.parentElement.parentElement;
+    const productPrice = parseFloat(productInfoDiv.querySelector("strong").innerText);
+
+    let productTotalPrice = productPrice * parseInt(quantityP.innerText);
+    console.log(productTotalPrice);
+    let productTotalDiv = productInfoDiv.querySelector(".product-line-price");
+    productTotalDiv.innerText = productTotalPrice.toFixed(2);
+    calculateCartTotal();
+
+}
+
+const calculateCartTotal = () =>{
+    var panel=document.querySelector("#panel");
+    let productTotalPrices = document.querySelectorAll(".product-line-price");
+    let subtotal = 0;
+    console.log(productTotalPrices);
+    productTotalPrices.forEach((productPrice)=>{
+        subtotal += parseFloat(productPrice.innerText);
+        
+    });
+    // let taxPrice = subtotal * taxRate; 
+    //localstorage deki değişkeni çektik.
+    let taxPrice = subtotal * parseFloat(localStorage.getItem("taxRate"));
+    let shipping = (subtotal > 0 ? shippingPrice : 0);
+    let cartTotal = subtotal + taxPrice + shipping;
+                                              //p  lerdeki 2.cocugu aldık  
+    document.querySelector("#cart-subtotal").lastElementChild.innerText = subtotal.toFixed(2);
+    document.querySelector("#cart-tax ").lastElementChild.innerText = taxPrice.toFixed(2);
+    document.querySelector("#cart-shipping ").lastElementChild.innerText = shipping.toFixed(2);
+    document.querySelector("#cart-total p:nth-child(2)").innerText = cartTotal.toFixed(2);
+    
+    
+}
+document.querySelectorAll(".remove-product").forEach((removeButton)=>{
+    removeButton.addEventListener("click", () =>{
+        removeProduct(removeButton);
+    });
+});
+
+const removeProduct = (removeButton) =>{
+    removeButton.parentElement.parentElement.parentElement.remove();
+    calculateCartTotal();
+}
+
+
+   
+
